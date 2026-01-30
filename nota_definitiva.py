431a7217cbbd02068cfc421934dc0bb72a60db71
JuanManuelDiazOchoa <servicios.technocore@gmail.com>
2026-01-30 06:47:06 -0500

    Se actualizo el README con análisis y diseño

--- nota_definitiva.py
+++ nota_definitiva.py
@@ -1,6 +1,33 @@
-#incio
-Nc = input("digite la nota cognitiva: ")
-Np = input("digite la nota procediental: ")
-Na = input("digite la nota actitudinal: ")
-Au = input("digite la nota autoevauacion: ")
-Bi = input("digite la nota bimestral: ")
+# Programa para calcular la nota definitiva de una asignatura
+
+#librerias
+import math
+
+#-----
+#input
+#-----
+
+print("                           ")
+print("      NOTA DEFINITIVA      ")
+print("                           ")
+
+Nc=int(input("digite la nota cognitiva: "))
+Np=int(input("digite la nota procediental: "))
+Na=int(input("digite la nota actitudinal: "))
+Au=int(input("digite la nota autoevauacion: "))
+Bi=int(input("digite la nota bimestral: "))
+
+#----------
+#Processing
+#----------
+nd=0.3*Nc+0.3*Np+0.2*Bi+0.1*Na+0.1*Au
+
+#------
+#output
+#------
+
+print("                      ")
+print("      RESULTADOS      ")
+print("                      ")
+print("La nota definitiva de la asignatura es: " +str(nd))
+print("                                                ")
